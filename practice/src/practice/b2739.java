6144a41aec30e5d950bcc23a2077a9d89e6f4edd
SilvieHong <122876238+SilvieHong@users.noreply.github.com>
2023-01-24 08:32:49 +0900

    2023-01-24

---
+++ practice/src/practice/b2739.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class b2739 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int i = sc.nextInt();
+
+		for (int j = 1; j < 10; j++) {
+			System.out.println(i + " * " + j + " = " + i * j);
+		}
+
+	}
+
+}
