090f4de0bf461fce22efb95caba8be8eb4fda22c
SilvieHong <122876238+SilvieHong@users.noreply.github.com>
2023-01-24 08:33:56 +0900

    2023-01-24

---
+++ practice/src/practice/b10950.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class b10950 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		int[][] num = new int[t][2];
+
+		for (int i = 0; i < t; i++) {
+			num[i][0] = sc.nextInt();
+			num[i][1] = sc.nextInt();
+		}
+		
+		for (int j = 0; j < t; j++) {
+			System.out.println(num[j][0] + num[j][1]);
+
+		}
+	}
+
+}
