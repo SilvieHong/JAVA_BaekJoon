b015fe7843bb7c5fd1928567d51bf9e5f55a055a
SilvieHong <122876238+SilvieHong@users.noreply.github.com>
2023-01-23 09:35:55 +0900

    2023-01-23

---
+++ practice/src/practice/b2884.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int h = sc.nextInt();
+		int m = sc.nextInt();
+		int t1 = h * 60 + m - 45;
+		int t2 = 24 * 60 + m - 45;
+
+		if (m >= 45) {
+			System.out.println(h + " " + (m - 45));
+		} else if (h == 0) {
+			System.out.println(t2 / 60 + " " + (t2 - (t2/60 * 60)));
+		} else {
+			System.out.println(t1 / 60 + " " + (t1 - (t1/60 * 60)));
+		}
+
+	}
+
+}
