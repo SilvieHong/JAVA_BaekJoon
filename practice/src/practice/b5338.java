0288c36220a47b63bea741c2734be3526b8b51d5
SilvieHong <122876238+SilvieHong@users.noreply.github.com>
2023-01-27 08:36:40 +0900

    2023-01-27

---
+++ practice/src/practice/b5338.java
@@ -0,0 +1,13 @@
+public class b5338 {
+
+	public static void main(String[] args) {
+	
+		System.out.println("       _.-;;-._");
+		System.out.println("'-..-'|   ||   |");
+		System.out.println("'-..-'|_.-;;-._|");
+		System.out.println("'-..-'|   ||   |");
+		System.out.println("'-..-'|_.-''-._|");
+		
+
+	}
+}
