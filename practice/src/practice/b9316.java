f576b508003f1b2f0672da8155a38b735a398c4c
SilvieHong <122876238+SilvieHong@users.noreply.github.com>
2023-01-28 10:24:47 +0900

    2023-01-28

---
+++ practice/src/practice/b9316.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class b9316 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		for (int i = 0; i < n; i++) {
+			System.out.println("Hello World, Judge " + (i + 1)+"!");
+		}
+
+	}
+}
