859d420e43da84b0e1d79977121f1664ddf549b4
SilvieHong <122876238+SilvieHong@users.noreply.github.com>
2023-01-28 10:21:18 +0900

    2023-01-28

---
+++ practice/src/practice/b6810.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class b6810 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+
+		int code = (9 * 1 + 7 * 3 + 8 * 1 + 0 * 3 + 9 * 1 + 2 * 3 + 1 * 1 + 4 * 3 + 1 * 1 + 8 * 3 + a * 1 + b * 3
+				+ c * 1);
+
+		System.out.println("The 1-3-sum is " + code);
+
+	}
+}
