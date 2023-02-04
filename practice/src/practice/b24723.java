c26754c8480eac2f35c41ed02df06ecaf1e54b15
SilvieHong <122876238+SilvieHong@users.noreply.github.com>
2023-02-04 16:26:52 +0900

    2023-02-04

---
+++ practice/src/practice/b24723.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class b24723 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int result = 1;
+
+		for (int i = 0; i < n; i++) {
+			result *= 2;
+		}
+		System.out.println(result);
+
+	}
+
+}
