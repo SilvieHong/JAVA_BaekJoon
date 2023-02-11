19530ee19cbb1d49e21c091bfe164c5dba397c04
SilvieHong <122876238+SilvieHong@users.noreply.github.com>
2023-02-11 16:03:04 +0900

    2023-02-11

---
+++ practice/src/practice/b5717.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class b5717 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		while (true) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			if (a == 0 || b == 0) {
+				break;
+			}
+			System.out.println(a + b);
+		}
+	}
+
+}
