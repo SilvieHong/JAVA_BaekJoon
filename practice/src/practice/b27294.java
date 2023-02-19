e0c16db099704d80aa9a7a2be2955efc164686a1
SilvieHong <122876238+SilvieHong@users.noreply.github.com>
2023-02-19 13:56:09 +0900

    2023-02-19

---
+++ practice/src/practice/b27294.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class b27294 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		int s = sc.nextInt();
+
+		
+		if (t <= 11) {
+			System.out.println(280);
+		} else if (t <= 16) {
+			if (s == 1) {
+				System.out.println(280);
+			} else {
+				System.out.println(320);
+			}
+
+		} else {
+			System.out.println(280);
+
+		}
+
+	}
+
+}
