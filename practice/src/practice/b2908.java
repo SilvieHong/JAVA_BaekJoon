22e74eff9c2fc09fca32d614a2518e7883df7975
SilvieHong <122876238+SilvieHong@users.noreply.github.com>
2023-02-22 13:09:31 +0900

    2023-02-22

---
+++ practice/src/practice/b2908.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class b2908 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String a = sc.next();
+		String b = sc.next();
+
+		String c = "";
+		String d = "";
+
+		for (int i = 2; i >= 0; i--) {
+			c += a.charAt(i);
+			d += b.charAt(i);
+		}
+		
+		if (Integer.parseInt(c) > Integer.parseInt(d)) {
+			System.out.println(c);
+		} else {
+			System.out.println(d);
+		}
+
+	}
+
+}
