f6d5c27b695196890498dad553d882fd54905ab4
SilvieHong <122876238+SilvieHong@users.noreply.github.com>
2023-03-12 09:18:04 +0900

    2023-03-12

--- practice/src/practice/b1152.java
+++ practice/src/practice/b1152.java
@@ -3,20 +3,26 @@ import java.util.Scanner;
 public class b1152 {
 
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
-		
-		String str1 = sc.nextLine();  	// 문장 입력 받기
-		String str2 = str1.trim(); 		// 문장 앞뒤의 공백 제거
-		
+
+		String str1 = sc.nextLine(); // 문장 입력 받기
+		String str2 = str1.trim(); // 문장 앞뒤의 공백 제거
+
 		int cnt = 0; // 공백 누적합계
-		
+
 		for (int i = 0; i < str2.length(); i++) {
 			if (str2.charAt(i) == ' ') {
 				cnt++;
 			}
 		}
-		System.out.println(cnt + 1);
+
+		if (str2.isEmpty()) {
+			System.out.println(0);
+		} else {
+			System.out.println(cnt + 1);
+		}
+
 	}
 
 }
